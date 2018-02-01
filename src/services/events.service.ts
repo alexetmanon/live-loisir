42828868c8d42ab84e8e243a4488ab68cc714c3f
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-02-01 11:19:42 +0100

    Fix typing with Event

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -3,13 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 
+import { Event } from '../models/event';
+
 const API_BASE = 'http://api.live-loisirs.alexandrebonhomme.fr';
 const EVENTS_END_POINT = '/events';
 
 @Injectable()
 export class EventsService {
 
-  private eventsSubject = new ReplaySubject<any>();
+  private eventsSubject = new ReplaySubject<Event[]>();
 
   constructor(private http: HttpClient) {
     this.refreshEvents();
@@ -19,7 +21,7 @@ export class EventsService {
     this.http.get(`${API_BASE}${EVENTS_END_POINT}`).subscribe(data => this.eventsSubject.next(data));
   }
 
-  getAll(page: number = 0, perPage: number = 10): Observable<any> {
+  getAll(page: number = 0, perPage: number = 10): Observable<Event[]> {
     return this.eventsSubject.asObservable();
   }
 }
