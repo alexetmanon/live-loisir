924e07520609ae88e8820206dfaea12e622bf88a
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-02-09 19:03:23 +0100

    Implements basic event details page

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 
 import { Event } from '../../models/event';
 import { ItineraryService } from '../../services/itinerary.service';
@@ -15,6 +15,7 @@ export class EventPage {
 
   event: Event;
   itineraries: any;
+  itineraryDirection: string = 'forward';
 
   constructor(
     navParams: NavParams,
@@ -30,17 +31,33 @@ export class EventPage {
           datetime: (new Date()).toISOString(),
           // beforeDatetime: true,
           sectionModes: [
-            TransportMode.Bike,
+            // TransportMode.Bike,
             TransportMode.Walking
           ],
           publicModes: [
-            PublicTransportMode.Tramway
+            PublicTransportMode.Tramway,
+            PublicTransportMode.Metro,
+            PublicTransportMode.Bus
           ]
         }
       )
       .subscribe(data => {
-        console.log(data)
-        this.itineraries = data
+        this.itineraries = data.journeys
       });
+  }
+
+  formatDuration(durationInSecond: number): string {
+    const duration = durationInSecond > 0 ? Math.round(durationInSecond / 60) : 0;
+
+    return `${duration} minute${duration > 1 ? 's' : ''}`;
+  }
+
+  formatPrice(fare: any): string {
+    let price = '0.0';
+    if (fare && fare.total && fare.total.value) {
+      price = fare.total.value;
+    }
+
+    return `${price} €`;
   }
 }
