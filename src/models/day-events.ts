74fb0fedee0f90d3894c7c7a90f5e0f96b44c348
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-02-14 20:21:49 +0100

    Adds a date field to DayEvent model

--- src/models/day-events.ts
+++ src/models/day-events.ts
@@ -2,6 +2,7 @@ import { Event } from "./event";
 
 export class DayEvents {
   constructor(
+    public date: Date,
     public dayName: string,
     public dayNumber: number,
     public events: Event[]
