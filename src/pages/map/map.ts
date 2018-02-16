8f02d4571dd70fa0025154df45de2873231da16d
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-02-16 15:19:56 +0100

    Implements refresh location

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { EventsService } from '../../services/events.service';
 import { Event } from '../../models/event';
 import { DayEvents } from '../../models/day-events';
-
+import { LatLng } from 'leaflet';
 
 @Component({
   selector: 'page-map',
@@ -14,9 +16,13 @@ export class MapPage {
   events: Event[];
   selectedEvent: Event;
 
+  mapCenter: LatLng;
+
   constructor(
-    eventsService: EventsService
-   ) {
+    eventsService: EventsService,
+    private geolocationService: Geolocation,
+    private toastController: ToastController
+  ) {
     eventsService.getAll().subscribe(data => this.dayEvents = data);
     eventsService.getDayEvents().subscribe(data => this.events = data);
   }
@@ -27,5 +33,20 @@ export class MapPage {
    */
   onMarkerClicked(event: Event): void {
     this.selectedEvent = event;
+  }
+
+  refreshLocation() {
+    this.geolocationService.getCurrentPosition().then(position => {
+      console.log(position)
+      this.mapCenter = new LatLng(position.coords.latitude, position.coords.longitude);
+    }).catch(error => {
+      console.log('Error getting location', error)
+
+      this.toastController.create({
+        message: 'Impossible récupérer votre position',
+        showCloseButton: true,
+        closeButtonText: 'Fermer'
+      }).present();
+    });
   }
 }
