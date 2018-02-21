3e1bdd0670ffe32d042d1b2958a0260a71055d5e
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-02-21 11:26:21 +0100

    Implements now page

--- src/pages/now/now.ts
+++ src/pages/now/now.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { App, NavController } from 'ionic-angular';
+import { EventsService } from '../../services/events.service';
+import { Event } from '../../models/event';
+import { EventPage } from '../event/event';
 
 @Component({
   selector: 'page-now',
   templateUrl: 'now.html'
 })
 export class NowPage {
+  private navController: NavController;
 
-  constructor(public navCtrl: NavController) {
+  events: Event[];
 
+  constructor(
+    eventsService: EventsService,
+    app: App
+  ) {
+    this.navController = app.getRootNavs()[0];
+
+    eventsService.getNowEvents().subscribe(data => this.events = data);
   }
 
+  onClick(event: Event): void {
+    this.navController.push(EventPage, {
+      event: event
+    });
+  }
 }
